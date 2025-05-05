150d2c1edb1833c15d64fd68ead0303449597513
Documenter.jl <documenter@juliadocs.github.io>
2025-05-05 21:23:28 +0000

    build based on 4fe7678

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MapBField2Surface","category":"page"},{"location":"#MapBField2Surface","page":"Home","title":"MapBField2Surface","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MapBField2Surface.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MapBField2Surface]","category":"page"}]
+[{"location":"#MapBField2Surface","page":"Home","title":"MapBField2Surface","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MapBField2Surface.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
 }
